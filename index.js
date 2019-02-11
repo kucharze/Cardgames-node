761c265f4c25c32cc23510718e4124d0de0f92b6
kucharze <kucharZ@duq.edu>
2019-02-10 23:07:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname,'/public')));
+
+//app.use('/resources',express.static(__dirname,'/Images'));
+
+// viewed at http://localhost:3000
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
