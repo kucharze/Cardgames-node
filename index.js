c32d41341bd077425b4cdaf4e8e8d9f97cd27548
kucharze <kucharZ@duq.edu>
2019-02-12 23:01:31 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,30 @@
-/*
+
+const SocketServer = require('ws').Server;
 var express = require('express');
-var app = express();
 var path = require('path');
-const WebSocket = require('ws');
-var mon = require('mongoose');
-
-https://devcenter.heroku.com/articles/node-websockets
-https://github.com/heroku-examples/node-websockets/blob/master/server.js
-//var dbUrl = ‘mongodb://username:pass@ds257981.mlab.com:57981/simple-chat’
-mongoose.connect(dbUrl , (err) => { 
-   console.log(‘mongodb connected’,err);
-})
-
-var Message = mongoose.model(‘Message’,{ name : String, message : String})
-
-
-//let socket = new WebSocket.Server({port:process.env.PORT || 8080}, () => {console.log("Server Started");});
-
+var connectedUsers = [];
+//init Express
+var app = express();
+//init Express Router
+var router = express.Router();
+var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname,'/public')));
 
-//app.use('/resources',express.static(__dirname,'/Images'));
-
-// viewed at http://localhost:3000
+//return static page with websocket client
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-app.listen(process.env.PORT || 8080, function(){});
-*/
-///*
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'public/index.html');
-
-express.static(path.join(__dirname,'/public'));
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+var server = app.listen(port, function () {
+    console.log('node.js static server listening on port: ' + port + ", with websockets listener")
+})
 const wss = new SocketServer({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+//init Websocket ws and handle incoming connect requests
+wss.on('connection', function connection(ws) {
+    console.log("connection ...");
+    //on connect message
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        connectedUsers.push(message);
+    });
+    ws.send('message from server at: ' + new Date());
 });
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
-//*/
