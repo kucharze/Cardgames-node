fe716353d57926e9b58449a2c8f1c11ba00972f1
kucharze <kucharZ@duq.edu>
2019-02-14 22:36:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app = express();
 var router = express.Router();
 var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname,'/public')));
+let webSockets=[];
 
 //return static page with websocket client
 app.get('/', function(req, res) {
@@ -20,11 +21,38 @@ var server = app.listen(port, function () {
 const wss = new SocketServer({ server });
 //init Websocket ws and handle incoming connect requests
 wss.on('connection', function connection(ws) {
-    console.log("connection ...");
+    console.log("Sucessful connection");
+    if(!webSockets.includes(ws)) {
+        webSockets.push(ws);
+        //webSockets[webSockets.indexOf(ws)].userNumber=clientCounter++;
+    }
     //on connect message
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-        connectedUsers.push(message);
+        let userMess = JSON.parse(message);
+        if(userMess.action="createlogin"){
+            createlogin(userMess);
+        }
+        if(userMess.action="login"){
+            login(userMess);
+        }
+        console.log('received: %s', userMess.user + " "+ userMess.password);
+        connectedUsers.push(userMess.user);
     });
-    ws.send('message from server at: ' + new Date());
+    //ws.send('message from server at: ' + new Date());
 });
+
+
+function createlogin(action){
+    console.log("Setting up a login");
+    console.log('received: %s', action.user + " "+ action.password);
+    let mes={};
+    mes.action="Succesfully set up a login "+action.user;
+    console.log("Loged in");
+    webSockets[0].send(JSON.stringify(mes));
+    
+}
+
+function login(action){
+    console.log('received: %s', "Attempting to log in");
+    console.log('received: %s', action.user + " "+ action.password);
+}
