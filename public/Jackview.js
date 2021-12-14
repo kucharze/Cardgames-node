8da922c9c8a4ed86d7d76ee18fffc96f12125c6d
Zachary Kuchar <zacharyk323@live.com>
2021-12-13 21:07:17 -0500

    Edited file locations for black jack files

---
+++ public/Jackview.js
@@ -0,0 +1,95 @@
+/**
+ * Provide methods that use the system I/O to interact with the user.
+ * This implementation uses the JavaScript window.prompt() method
+ * for input (and some output) and window.alert() for (purely) output.
+ */
+class Jackview {
+    constructor(jpres) {
+    this.jpres = jpres;
+    this.topCard = null;
+    this.topCardString = "";
+  }
+
+  displayComputerHand(hand){
+     let cpu = document.querySelector("#dealerhand");
+     while(cpu.hasChildNodes()){
+	 cpu.removeChild(cpu.lastChild);
+      }
+  for(let i=0; i<hand.length; i++){
+      let image=document.createElement("img");
+      if(!hand[i].flipped){
+	       image.src ="./Images/"+hand[i].toString()+".png";
+	       image.title=hand[i].toString();
+	       image.class="card positionable";
+	       image.style="position:absolute; left:"+  ((35*(i))+10) + "px; z-index:" + i +" hieght:10px";
+      }else{
+	    image.src ="./Images/cardback.png";
+	    image.title=hand[i].toString();
+	    image.class="card positionable";
+	    image.style="position:absolute; left:"+  ((35*(i))+10) + "px; z-index:" + i +" hieght:10px";
+      }
+	   cpu.appendChild(image);
+   }
+  }
+
+  displayHumanHand(hand){
+     let human = document.querySelector("#playerhand");
+     while(human.hasChildNodes()){
+	 human.removeChild(human.lastChild);
+      }
+     for(let i=0; i<hand.length; i++){
+        let image=document.createElement("img");
+	   image.src ="./Images/"+hand[i].toString()+".png";
+	   image.title=hand[i].toString();
+	   image.class="card positionable";
+	   image.style="position:absolute; left:"+  ((35*(i))+10) + "px; z-index:" + i +" hieght:10px";
+	   human.appendChild(image);
+   }
+  }
+    
+    addComCard(card,numCards){
+        let cpu = document.getElementById("dealerhand");
+       //alert("cpu");
+       let image=document.createElement("img");
+	   image.src ="./Images/"+card.toString()+".png";
+	   image.title=card.toString()+"E";
+        //image.id=card.toString()+"E";
+	   image.class="card positionable";
+	   image.style="position:absolute; left:"+  (-30) + "px; z-index:" + numCards +" hieght:10px";
+	   cpu.appendChild(image);
+        this.moveCard(image,(35*(numCards-1)),-30);
+    }
+    
+    addHumanCard(card,numCards){//Have a card move in from offscreen
+       let human=document.getElementById("playerhand");
+       let image=document.createElement("img");
+	   image.src ="./Images/"+card.toString()+".png";
+	   image.title=card.toString()+"J";
+       image.id=card.toString()+"J";
+	   image.class="card positionable";
+	   image.style="position:absolute; left:"+  -35 + "px; z-index:" + i +" hieght:10px";
+        human.appendChild(image);
+        this.moveCard(image,(35*(numCards-1)));
+    }
+    
+    moveCard(image,pos){//for handling card animations
+        $(image).animate({left: pos},800);
+    }
+    
+    displayMessage(message){
+        let mes=document.getElementById("jackstatus");
+        mes.innerHTML=message;
+    }
+    
+    eraseHands(){
+        let human=document.getElementById("playerhand");
+        let computer=document.getElementById("dealerhand");
+        while(human.hasChildNodes()){
+            human.removeChild(human.lastChild);
+        }
+        while(computer.hasChildNodes()){
+	       computer.removeChild(computer.lastChild);
+        }
+    }
+
+}
