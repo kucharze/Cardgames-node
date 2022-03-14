e31581bf4897036a47d7c603fb722e3f6fbd2565
Zachary Kuchar <zacharyk323@live.com>
2022-03-13 22:24:24 -0400

    Created new Class to handle login information

---
+++ public/Online/LoginHandler.js
@@ -0,0 +1,16 @@
+///*
+if (typeof Uploader === "undefined") {
+    Card = require('./LoginHandler');
+  }
+  //*/
+class LoginHandler{
+
+    constructor(){
+        //this.s="hello";
+    }
+
+
+}
+if (typeof module === "object") {
+    module.exports = LoginHandler;
+ }
